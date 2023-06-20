6bf9a7984ce3753324debca9772c49e444dcb03e
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-20 23:56:31 +0200

    initial commit & tailwind setup + dark mode

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <h1 class="text-red-500 text-4xl dark:text-dim-500">Clone</h1>
+  </div>
+</template>
+<script setup>
+const darkMode = ref(true);
+</script>
