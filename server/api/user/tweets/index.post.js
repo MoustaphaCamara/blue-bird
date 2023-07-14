44ac930f3ae0249933c8bdfffa27cdce122912ea
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-07-14 19:13:18 +0200

    tweet components

--- server/api/user/tweets/index.post.js
+++ server/api/user/tweets/index.post.js
@@ -1,4 +1,7 @@
 import formidable from "formidable";
+import { createTweet } from "~/server/db/createTweet";
+import { createMediaFile } from "~/server/db/mediaFiles";
+import { tweetTransformer } from "~/server/transformers/tweet";
 
 export default defineEventHandler(async (event) => {
   const form = formidable({});
@@ -11,7 +14,37 @@ export default defineEventHandler(async (event) => {
       resolve({ fields, files });
     });
   });
+
+  const { fields, files } = response;
+
+  const userId = event.context?.auth?.user?.id;
+
+  const tweetData = {
+    text: fields.text.toString(),
+    //https://github.com/prisma/prisma/discussions/2677
+    authorId: userId,
+  };
+  const tweet = await createTweet(tweetData);
+
+  const filePromises = Object.keys(files).map(async (key) => {
+    const file = files[key];
+
+    const response = await uploadToCloudinary(file[0].filepath);
+
+    console.log(response);
+
+    return createMediaFile({
+      url: "",
+      providerPublicId: "random_id",
+      userId: userId,
+      tweetId: tweet.id,
+    });
+  });
+
+  await Promise.all(filePromises);
+
   return {
-    hello: response,
+    // tweet: tweetTransformer(tweet),
+    files,
   };
 });
